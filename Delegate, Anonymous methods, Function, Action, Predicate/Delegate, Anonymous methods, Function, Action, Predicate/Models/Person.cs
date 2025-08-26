380fb0b69b6bdfe084b204b67dcad30e28f40061
SubhanHuseynv <subhanshh@code.edu.az>
2025-08-26 17:38:14 +0400

    initial

---
+++ Delegate, Anonymous methods, Function, Action, Predicate/Delegate, Anonymous methods, Function, Action, Predicate/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate__Anonymous_methods__Function__Action__Predicate.Models
+{
+    internal class Person
+    {
+        public string Name{ get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Delegate, Anonymous methods, Function, Action, Predicate/Delegate, Anonymous methods, Function, Action, Predicate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Delegate__Anonymous_methods__Function__Action__Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Models.Person> people = new List<Models.Person>()
+            {
+                new Models.Person(){ Name="John", Surname="Doe", Age=30},
+                new Models.Person(){ Name="Jane", Surname="Smith", Age=25},
+                new Models.Person(){ Name="Sam", Surname="Brown", Age=40},
+                new Models.Person(){ Name="Lucy", Surname="Johnson", Age=35},
+            };
+           people.ForEach(p => Console.WriteLine($"{p.Name} {p.Surname}, Age: {p.Age}"));
+            people.FindAll(p => p.Name > "John").forEach(p => Console.WriteLine($"{p.Name} {p.Surname}, Age: {p.Age}"));
+            people.FindAll(p => (p.Name.EndWith("ov") || p.Name.EndWith("ova"))).forEach(p => Console.WriteLine($"{p.Name} {p.Surname}, Age: {p.Age}"));
+            people.FindAll(p=>p.Age>20).forEach(p => Console.WriteLine($"{p.Name} {p.Surname}, Age: {p.Age}"));
+        }
+        
+    }
+}
